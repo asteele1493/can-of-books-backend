8f7b0c3e86e5910736493d544f5147244e47a730
Andra <94204279+asteele1493@users.noreply.github.com>
2022-10-25 21:50:14 -0700

    Started lab12

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,8 +5,9 @@
 //need our book model
 
 const Book = require('../models/book');
+const Handlers = {};
 
-const getBooks = async(request, response, next) => {
+Handlers.getBooks = async(request, response, next) => {
   try{
   //books variable is going to hold all of our books in our database
   //passing in an empty object tells mongoose to get all the documents from the database.
@@ -14,9 +15,25 @@ const getBooks = async(request, response, next) => {
   response.status(200).send(books);
   }
   catch(error) {
-    console.log(error);
+    error.customMessage = 'Could not get books: ';
+    console.log(error.customMessage + error);
     next(error);
   }
 }
 
-module.exports = getBooks;
+Handlers.createBooks = async(request, response, next) => {
+  try{
+  const books = await Book.create(request.body);
+  response.status(201).send(books);
+  }
+  catch(error) {
+    error.customMessage = 'Could not create new book: ';
+    console.log(error.customMessage + error);
+    next(error);
+  }
+}
+
+module.exports = Handlers;
+
+
+//Notes to look up: check customMessage syntax. Do we need to declare?
